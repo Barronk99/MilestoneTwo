2f32b9e19cf3a4d6e237b00ac934e528ae727a6c
Barronk99 <keralib@yahoo.co.uk>
2021-06-30 17:28:25 +0000

    Implemented code for the 'win' function so the score updates and the game returns a message declaring the winner

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const userScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const resulst_div = document.querySelector(".result");
+const resulst_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,8 +14,11 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-function win() {
-    console.log("WIN");
+function win(user, computer) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    resulst_p.innerHTML = userChoice + "beats" + computerChoice + "You win!";
 }
 
 function lose() {
@@ -32,17 +35,17 @@ function game (userChoice) {
         case "rp":
         case "pr":
         case "sp":
-            win();
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            lose();
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            draw();
+            draw(userChoice, computerChoice);
             break;
     }
 }
