a63796764e3afb32a9a7546f75b75984634e4189
Barronk99 <keralib@yahoo.co.uk>
2021-06-30 15:06:58 +0000

    Added transition to choice 2 and 3. Tested JS by creating simple event listener and logging to the console when one choice is clicked.

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,15 @@ const resulst_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener('click', function() {
+    console.log("Great! You clicked Rock");
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("Great! You clicked Paper");
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log("Great! You clicked Scissors");
+})
