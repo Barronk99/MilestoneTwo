b54434fd57900f08c9afba894e8dc9ae218ee850
Barronk99 <keralib@yahoo.co.uk>
2021-06-30 15:35:03 +0000

    added a function called 'main' to run the code to select a choice

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,9 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+
+function main() {
+
 rock_div.addEventListener('click', function() {
     console.log("Great! You clicked Rock");
 })
@@ -19,3 +22,6 @@ paper_div.addEventListener('click', function() {
 scissors_div.addEventListener('click', function() {
     console.log("Great! You clicked Scissors");
 })
+}
+
+main();
